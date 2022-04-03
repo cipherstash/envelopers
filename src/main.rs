22c370627acf26b7d8bccf0ed70eebb1dad9f8d9
Dan Draper <dan@coderdan.co>
2022-04-03 20:41:44 +1000

    Basic decryption

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use envelope::{EnvelopeCipher, KeyProvider, SimpleKeyProvider};
+use envelope::{EnvelopeCipher, SimpleKeyProvider};
 
 /* TODO
  * - KMS Implementation
  * - Version header
- * - Serde
+ * - Serde (for ciphertexts. BSON plaintexts to vector should be in a separate library)
  * - Key caching
- * - Decryption
- * - Bulk encrypt (could be used in conjunction with keu caching)
+ * - Zeroize
+ * - Bulk encrypt (could be used in conjunction with key caching)
  *   - i.e. encrypt a bunch of records with 1 data-key
  */
 
@@ -17,4 +17,8 @@ fn main() {
     let er = cipher.encrypt(b"hey there monkey boy").unwrap();
 
     println!("ER: {:?}", er);
+
+    let pt = cipher.decrypt(er).unwrap();
+
+    println!("PT {:?}", std::str::from_utf8(&pt).unwrap());
 }
