e1954d252cf7465c893de99d4986d226cfb646b5
Bennett Hardwick <bennett@cipherstash.com>
2022-04-14 13:18:07 +1000

    Add KMS key provider tests

--- src/kms_key_provider.rs
+++ src/kms_key_provider.rs
@@ -39,7 +39,9 @@ impl KeyProvider for KMSKeyProvider {
             .key_spec(self.data_key_spec.clone())
             .send()
             .await
-            .map_err(|e| KeyGenerationError::Other(e.to_string()))?;
+            .map_err(|e| {
+                KeyGenerationError::Other(format!("KMS generate data key request failed: {}", e))
+            })?;
 
         let encrypted_key = response
             .ciphertext_blob
@@ -54,7 +56,7 @@ impl KeyProvider for KMSKeyProvider {
         })?;
 
         let plaintext_blob = response.plaintext.ok_or_else(|| {
-            KeyGenerationError::Other(String::from("Response did not contain key plaintext key"))
+            KeyGenerationError::Other(String::from("Response did not contain plaintext key"))
         })?;
 
         let key = Key::clone_from_slice(plaintext_blob.as_ref());
@@ -83,5 +85,198 @@ impl KeyProvider for KMSKeyProvider {
         })?;
 
         Ok(Key::clone_from_slice(plaintext_blob.as_ref()))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use tokio;
+
+    use aws_sdk_kms::{Client, Config, Credentials, Region};
+    use aws_smithy_client::test_connection::TestConnection;
+    use aws_smithy_http::body::SdkBody;
+    use base64::encode;
+    use core::future::Future;
+    use http::{Request, Response, StatusCode};
+
+    use crate::{KMSKeyProvider, KeyProvider};
+
+    async fn with_mocked_response<C, F>(
+        request_body: impl Into<String>,
+        response_body: impl Into<String>,
+        response_code: u16,
+        callback: C,
+    ) where
+        C: FnOnce(Client) -> F,
+        F: Future<Output = ()>,
+    {
+        let creds = Credentials::new(
+            "ANOTREAL",
+            "notrealrnrELgWzOk3IfjzDKtFBhDby",
+            Some("notarealsessiontoken".to_string()),
+            None,
+            "test",
+        );
+
+        let conn = TestConnection::new(vec![(
+            Request::builder()
+                .uri("https://kms.ap-southeast-2.amazonaws.com")
+                .body(SdkBody::from(request_body.into()))
+                .expect("Failed to create request body"),
+            Response::builder()
+                .status(StatusCode::from_u16(response_code).expect("Invalid status code"))
+                .body(response_body.into())
+                .expect("Failed to create response body"),
+        )]);
+
+        let conf = Config::builder()
+            .region(Region::new("ap-southeast-2"))
+            .credentials_provider(creds)
+            .build();
+
+        let client = Client::from_conf_conn(conf, conn.clone());
+
+        callback(client).await;
+
+        assert_eq!(conn.requests().len(), 1);
+        conn.assert_requests_match(&[]);
+    }
+
+    #[tokio::test]
+    async fn test_generate_data_key() {
+        let key_id = "test-key-id";
+        let plaintext: Vec<u8> = vec![1; 16];
+        let ciphertext: Vec<u8> = vec![2; 16];
+
+        with_mocked_response(
+            r#"{"KeyId":"test-key-id","KeySpec":"AES_128"}"#,
+            format!(
+                r#"{{"CiphertextBlob":"{}","Plaintext":"{}","KeyId":"{}"}}"#,
+                encode(&ciphertext),
+                encode(&plaintext),
+                key_id
+            ),
+            200,
+            |client| async move {
+                let provider = KMSKeyProvider::new(client, key_id.into());
+
+                let key = provider
+                    .generate_data_key()
+                    .await
+                    .expect("Failed to generate data key");
+
+                assert_eq!(key.key.as_slice(), plaintext.as_slice());
+                assert_eq!(key.encrypted_key.as_slice(), ciphertext.as_slice());
+                assert_eq!(key.key_id, key_id);
+            },
+        )
+        .await;
+    }
+
+    #[tokio::test]
+    async fn test_generate_missing_encrypted_key_response() {
+        let key_id = "test-key-id";
+
+        with_mocked_response(
+            r#"{"KeyId":"test-key-id","KeySpec":"AES_128"}"#,
+            "{}",
+            200,
+            |client| async move {
+                let provider = KMSKeyProvider::new(client, key_id.into());
+
+                let result = provider.generate_data_key().await;
+
+                assert_eq!(
+                    result
+                        .map_err(|e| e.to_string())
+                        .expect_err("Expected result to be error"),
+                    String::from("Response did not contain encrypted key")
+                );
+            },
+        )
+        .await;
+    }
+
+    #[tokio::test]
+    async fn test_generate_missing_plaintext_key_response() {
+        let key_id = "test-key-id";
+        let ciphertext: Vec<u8> = vec![2; 16];
+
+        with_mocked_response(
+            r#"{"KeyId":"test-key-id","KeySpec":"AES_128"}"#,
+            format!(
+                r#"{{"CiphertextBlob":"{}","KeyId":"{}"}}"#,
+                encode(&ciphertext),
+                key_id
+            ),
+            200,
+            |client| async move {
+                let provider = KMSKeyProvider::new(client, key_id.into());
+
+                let result = provider.generate_data_key().await;
+
+                assert_eq!(
+                    result
+                        .map_err(|e| e.to_string())
+                        .expect_err("Expected result to be error"),
+                    String::from("Response did not contain plaintext key")
+                );
+            },
+        )
+        .await;
+    }
+
+    #[tokio::test]
+    async fn test_generate_missing_key_id_response() {
+        let key_id = "test-key-id";
+        let ciphertext: Vec<u8> = vec![2; 16];
+        let plaintext: Vec<u8> = vec![1; 16];
+
+        with_mocked_response(
+            r#"{"KeyId":"test-key-id","KeySpec":"AES_128"}"#,
+            format!(
+                r#"{{"CiphertextBlob":"{}","PlaintextKey":"{}"}}"#,
+                encode(&ciphertext),
+                encode(&plaintext)
+            ),
+            200,
+            |client| async move {
+                let provider = KMSKeyProvider::new(client, key_id.into());
+
+                let result = provider.generate_data_key().await;
+
+                assert_eq!(
+                    result
+                        .map_err(|e| e.to_string())
+                        .expect_err("Expected result to be error"),
+                    String::from("Response did not contain key_id")
+                );
+            },
+        )
+        .await;
+    }
+
+    #[tokio::test]
+    async fn test_generate_bad_request() {
+        let key_id = "test-key-id";
+
+        with_mocked_response(
+            r#"{"KeyId":"test-key-id","KeySpec":"AES_128"}"#,
+            "{}",
+            500,
+            |client| async move {
+                let provider = KMSKeyProvider::new(client, key_id.into());
+
+                let result = provider.generate_data_key().await;
+
+                assert_eq!(
+                    result
+                        .map_err(|e| e.to_string())
+                        .expect_err("Expected result to be error"),
+                    String::from("KMS generate data key request failed: Error")
+                );
+            },
+        )
+        .await;
     }
 }
