5b221368fe5ca3df9738307b496eabdaf96da3c1
Bennett Hardwick <bennett@cipherstash.com>
2022-08-30 15:34:28 +1000

    Update KeyProvider, make CachingKeyWrapper a provider

--- examples/kms.rs
+++ examples/kms.rs
@@ -1,5 +1,5 @@
 use aws_sdk_kms::Client;
-use envelopers::{CacheOptions, EnvelopeCipher, KMSKeyProvider};
+use envelopers::{CacheOptions, CachingKeyWrapper, EnvelopeCipher, KMSKeyProvider};
 use std::error::Error;
 use std::time::Duration;
 
@@ -21,14 +21,15 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let provider = KMSKeyProvider::new(client, std::env::var("CS_KEY_ID")?);
 
-    let cipher: EnvelopeCipher<KMSKeyProvider> = EnvelopeCipher::init(
-        provider,
-        CacheOptions::default()
-            .with_max_age(Duration::from_secs(30))
-            .with_max_bytes(100 * 1024)
-            .with_max_messages(10)
-            .with_max_entries(10),
-    );
+    let cipher: EnvelopeCipher<CachingKeyWrapper<KMSKeyProvider>> =
+        EnvelopeCipher::init(CachingKeyWrapper::new(
+            provider,
+            CacheOptions::default()
+                .with_max_age(Duration::from_secs(30))
+                .with_max_bytes(100 * 1024)
+                .with_max_messages(10)
+                .with_max_entries(10),
+        ));
 
     let encrypted = cipher
         .encrypt("This is a great test string!".as_bytes())
